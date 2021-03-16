7f10b954229093bfa970fb082055a5694da2cf67
EunhaKyeong <eh0634a@gmail.com>
2021-03-16 12:23:52 +0900

    ProductList.js 리팩토링중 - 현재까지 필요 없는 코드, 페이징 처리 때 서버에 전달하는 데이터 page, categoryCode -> page로 변경함

--- src/main/webapp/resources/ProductList/js/ProductList.js
+++ src/main/webapp/resources/ProductList/js/ProductList.js
@@ -1,16 +1,10 @@
-//초기페이지 히스토리 저장
-/*$(document).ready(function() {
-	var productHtml = $("#productRow").html();
-	var buttonHtml = $("#pageButtonGroup").html();
-	
-	history.pushState({"productHtml":productHtml, "buttonHtml":buttonHtml}, null, null);
-});*/
-var categoryCode = Number($(location).attr('pathname').slice(-1));
-var totalPage = Math.ceil($.cookie("productCnt")/6);
+var categoryCode = $(location).attr('pathname').slice(-1);	//현재 페이지 카테고리 코드
+var totalPage = Math.ceil($.cookie("productCnt")/6);	//총 페이지 ex)의류 카테고리에 10개의 상품이 있다면 총 2 페이지가 필요하다.
 var buttonHtml = "";
+var data;
 
 $(window).ready(function() {
-	var data = {"page":1, "categoryCode":categoryCode};
+	data = {"page":1};	//서버에 전달할 데이터
 	
 	if (totalPage>5) {
 		for (var page=1; page<=5; page++) {
@@ -26,12 +20,6 @@ $(window).ready(function() {
 	$(".pageButton").first().css({background:"black", color:"white"});
 	
 	pagingProduct(data);
-	/*pagingDone(data);
-	
-	if (totalPage>click+1) {
-		html = "<button type=\"button\" class=\"btn btn-secondary btn-sm btn-next\">다음</button>";
-	}
-	$(".paging-container").html(html);*/
 });
 
 //페이지버튼 클릭했을 때
@@ -39,41 +27,13 @@ $(document).on("click", ".pageButton", function() {
 	$(".pageButton").css({background:"#F8F9FA", color:"black"});
 	$(this).css({background:"black", color:"white"});
 	
-	//var index = $(this).attr("index");	//index 속성값
-	var page = Number($(this).text());
-	//var categoryCode = Number($(location).attr('pathname').slice(-1));
-	var data = {"page":page,
-				"categoryCode":categoryCode};
-	
+	data = {"page":Number($(this).text())};
 	pagingProduct(data);
-	/*$.ajax({
-		url:"/ProductList/paging", 
-		type:"POST", 
-		data:JSON.stringify(dataTransfer),
-		contentType: "application/json; charset=utf-8;",
-	    dataType: "json",
-		success:function(data) {	
-			//클릭된 버튼 강조된 스타일로 변경
-			$(".pageButton[index=" + index + "]").addClass("clicked_pageButton");
-			//클릭된 버튼을 제외하고는 클릭되기 전으로 변경하기
-			$(".pageButton[index!=" + index + "]").removeClass("clicked_pageButton");
-			
-			//해당페이지 상품리스트 정보들을 담은 html을 jsp에 추가하는 함수 호출
-			paging(data);
-			
-		},
-		error:function() {
-			alert("ERROR");
-		}
-	});*/
 });
 
 //다음페이지 클릭했을 때
 $(document).on("click", "#nextButton", function() {
-	var data = {"page":Number($(".pageButton").last().text())+1,
-				"categoryCode":categoryCode};
-	/*var data = {"page":Number($(".pageButton")[4].textContent),
-				"categoryCode":categoryCode};*/
+	data = {"page":Number($(".pageButton").last().text())+1};
 				
 	buttonHtml = "<button id=\"prevButton\" type=\"button\" class=\"btn btn-light\">이전</button>";
 	if (totalPage>=data.page+5) {
@@ -90,53 +50,11 @@ $(document).on("click", "#nextButton", function() {
 	$(".pageButton").first().css({background:"black", color:"white"});
 	
 	pagingProduct(data);
-	/*$.ajax({
-		url:"/ProductList/nextButton", 
-		type:"POST", 
-		data:JSON.stringify(dataTransfer), 
-		contentType: "application/json; charset=utf-8;",
-	    dataType: "json", 
-		success:function(data) {
-			//버튼 새로고침
-			var buttonHtml = "";
-			var totalPage = data.totalPage;
-			buttonHtml += "<button id=\"prevButton\" type=\"button\" class=\"btn btn-light\">이전</button>";
-			if (startPage+4>=totalPage) {
-				for (var page=startPage; page<=totalPage; page++) {
-					buttonHtml += "<button type=\"button\" class=\"btn btn-light pageButton\" index=\"" + page%5 + "\">" + page + "</button>";
-				}
-			}
-			else {
-				for (var page=startPage; page<=startPage+4; page++) {
-					buttonHtml += "<button type=\"button\" class=\"btn btn-light pageButton\" index=\"" + page%5 + "\">" + page + "</button>";
-				}
-				buttonHtml += "<button id=\"nextButton\" type=\"button\" class=\"btn btn-light\">다음</button>";
-			}
-			
-			$("#pageButtonGroup").html(buttonHtml);
-			
-			// 다음페이지의 첫번째 페이지 버튼 클릭된 상태 스타일로 변경
-			$(".pageButton[index=1]").addClass("clicked_pageButton");
-			
-			//해당페이지 상품리스트 정보들을 담은 html을 jsp에 추가하는 함수 호출
-			paging(data.productList);
-		}, 
-		error:function() {
-			alert("ERROR!!");
-		}
-	});*/
 });
 
 //이전버튼 클릭했을 때
 $(document).on("click", "#prevButton", function() {
-	/*var lastPage = Number($(".pageButton")[0].textContent)-1;
-	var categoryCode = Number($(location).attr('pathname').slice(-1));
-	var dataTransfer = {"startIdx":6*(lastPage-1),
-						"categoryCode":categoryCode};*/
-	var data = {"page":Number($(".pageButton").first().text())-1,
-				"categoryCode":categoryCode};
-	/*var data = {"page":Number($(".pageButton")[0].textContent)-2,
-				"categoryCode":categoryCode};*/
+	data = {"page":Number($(".pageButton").first().text())-1};
 				
 	buttonHtml = "";
 	if (data.page-5!=0) {
@@ -146,49 +64,14 @@ $(document).on("click", "#prevButton", function() {
 		buttonHtml += "<button type=\"button\" class=\"btn btn-light pageButton\" index=\"" + page + "\">" + page + "</button>";
 	}
 	buttonHtml += "<button id=\"nextButton\" type=\"button\" class=\"btn btn-light\">다음</button>";
-			
 	$("#pageButtonGroup").html(buttonHtml);
 	$(".pageButton").last().css({background:"black", color:"white"});
 	
-	pagingProduct(data);			
-	/*$.ajax({
-		url:"/ProductList/paging", 
-		type:"POST", 
-		data:JSON.stringify(dataTransfer), 
-		contentType:"application/json; charset=utf-8", 
-		dataType:"json", 
-		success:function(data) {
-			//버튼 새로고침
-			var buttonHtml = "";
-			if (lastPage-4==1) {
-				for (var page=1; page<=lastPage; page++) {
-					buttonHtml += "<button type=\"button\" class=\"btn btn-light pageButton\" index=\"" + page%5 + "\">" + page + "</button>";
-				}
-			}
-			else {
-				buttonHtml += "<button id=\"prevButton\" type=\"button\" class=\"btn btn-light\">이전</button>";
-				for (var page=lastPage-4; page<=lastPage; page++) {
-					buttonHtml += "<button type=\"button\" class=\"btn btn-light pageButton\" index=\"" + page%5 + "\">" + page + "</button>";
-				}
-			}
-			buttonHtml += "<button id=\"nextButton\" type=\"button\" class=\"btn btn-light\">다음</button>";
-			
-			$("#pageButtonGroup").html(buttonHtml);
-			
-			//이전페이지로 돌아간 후 맨 마지막 페이지 버튼 클릭된 상태 스타일로 변경
-			$(".pageButton[index=0]").addClass("clicked_pageButton");
-			
-			//해당페이지 상품리스트 정보들을 담은 html을 jsp에 추가하는 함수 호출
-			paging(data);
-		}, 
-		error:function() {
-			alert("ERROR!!");
-		}
-	});*/
+	pagingProduct(data);		
 });
 
 function pagingProduct(data) {
-	axios.post("/ProductList/paging", data).then(function(res) {
+	axios.post("/ProductList/paging/" + categoryCode, data).then(function(res) {
 		var html = "";
 		var imageSrc = "";
 		for (var i=0; i<res.data.length; i++) {
@@ -204,7 +87,6 @@ function pagingProduct(data) {
 			}
 			html += "<div class=\"col-lg-4 col-md-6 mb-4\">";
 			html += "<div class=\"card h-100\">";
-			//html += "<a href=\"/ProductDetail/" + data[i].product_code + "\"><img class=\"card-img-top\" src=\"http://placehold.it/700x400\" alt=\"\"></a>";
 			html += "<a href=\"/ProductDetail/" + res.data[i].product_code + "\"><img class=\"card-img-top\" src=" + imageSrc + " alt=\"\"></a>";
 			html += "<div class=\"card-body\">";
 			html += "<h4 class=\"card-title\">";
@@ -224,6 +106,14 @@ function pagingProduct(data) {
 		//ProductList 페이지에서 뒤로가기 버튼을 눌렀을 때 이전 페이지의 history를 기억하기 위해 pushState() 사용.(Detail 페이지에서 뒤로가기 버튼 누른거랑 다른거)
 		var buttonHtml = $("#pageButtonGroup").html();
 		history.pushState({"productHtml":html, "buttonHtml":buttonHtml}, null, null);
+		
+		//상품 상세 정보에서 뒤로 가기 버튼
+		$('#productRow').find('a').each(function(index) {
+			$(this).click(function(e) {
+				
+			});
+		});
+		
 	}).catch(function(error) {
 		alert("오류가 발생했습니다!");
 		if (error.response) {
@@ -242,48 +132,9 @@ function pagingProduct(data) {
 	});
 }
 
-//해당페이지 상품리스트 정보들을 담은 html을 jsp에 추가하는 함수
-/*function paging(data) {
-	var html = "";
-	var imageSrc = "";
-	for (var i=0; i<data.length; i++) {
-		var productStockHtml = "";
-		if (data[i].thumbnail_url=='none.png') {
-			imageSrc = "https://www.namdokorea.com/site/jeonnam/tour/images/noimage.gif";
-		}
-		else {
-			imageSrc = data[i].thumbnail_url;
-		}
-		if (data[i].product_stock==0) {
-			productStockHtml = "<p class=\"card-text\" style=\"color:red\"> 품절된 상품입니다. </p>";
-		}
-		html += "<div class=\"col-lg-4 col-md-6 mb-4\">";
-		html += "<div class=\"card h-100\">";
-		//html += "<a href=\"/ProductDetail/" + data[i].product_code + "\"><img class=\"card-img-top\" src=\"http://placehold.it/700x400\" alt=\"\"></a>";
-		html += "<a href=\"/ProductDetail/" + data[i].product_code + "\"><img class=\"card-img-top\" src=" + imageSrc + " alt=\"\"></a>";
-		html += "<div class=\"card-body\">";
-		html += "<h4 class=\"card-title\">";
-		html += "<a href=\"/ProductDetail/" + data[i].product_code + "\">" + data[i].product_name + "</a>";
-		html += "</h4>";
-		html += "<h5>" + data[i].product_price + "원</h5>";
-		html += productStockHtml;
-		html += "<p class=\"card-text\">" + data[i].product_manufacturer + "</p>";
-		html += "</div>";
-		html += "<div class=\"card-footer\">" + data[i].product_score + "</div>";
-		html += "</div>";
-		html += "</div>";
-	}
-			
-	$("#productRow").html(html);
-	
-	//ProductList 페이지에서 뒤로가기 버튼을 눌렀을 때 이전 페이지의 history를 기억하기 위해 pushState() 사용.(Detail 페이지에서 뒤로가기 버튼 누른거랑 다른거)
-	var buttonHtml = $("#pageButtonGroup").html();
-	history.pushState({"productHtml":html, "buttonHtml":buttonHtml}, null, null);
-};*/
-
 //ProductList 페이지에서 뒤로가기버튼을 눌렀을 때(Detail 페이지에서 뒤로가기 버튼 누른거랑 다른거)
 $(window).on('popstate',function(){
-	var data = history.state;
+	data = history.state;
 	$("#productRow").html(data.productHtml);
 	$("#pageButtonGroup").html(data.buttonHtml);
-}) ;
+});
